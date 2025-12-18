0129af715984f6f6695adcac99ff33d299c3cc29
carlcidromero <carlcidromero@gmail.com>
2025-12-18 03:39:50 -0800

    feat: consume remote css

--- scripts/build/prod.js
+++ scripts/build/prod.js
@@ -6,9 +6,11 @@ const OUTPUT_FILE = "index.html";
 const OUTPUT_PATH = `${OUTPUT_DIRECTORY}/${OUTPUT_FILE}`;
 const ENCODING = "utf8";
 const ABOUT_ME_REMOTE = "content/pages/home/about-me.md";
-const CSS_PATH = "index.css";
+const CSS_REMOTE = "ui/styles/output.css";
+const CSS_OUTPUT_FILE = "index.css";
 
 const markdown = fs.readFileSync(ABOUT_ME_REMOTE, ENCODING);
+const css = fs.readFileSync(CSS_REMOTE, ENCODING);
 const parsed = marked.parse(markdown);
 
 const page = (html) => `<!doctype html>
@@ -27,5 +29,5 @@ const page = (html) => `<!doctype html>
 
 fs.mkdirSync(OUTPUT_DIRECTORY, { recursive: true });
 
-fs.copyFileSync(CSS_PATH, `${OUTPUT_DIRECTORY}/${CSS_PATH}`);
+fs.writeFileSync(`${OUTPUT_DIRECTORY}/${CSS_OUTPUT_FILE}`, css);
 fs.writeFileSync(OUTPUT_PATH, page(parsed));
