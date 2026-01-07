b064546991e4a75e187f2098f4ce901086e54f01
carlcidromero <carlcidromero@gmail.com>
2026-01-06 23:41:55 -0800

    fix: writeBookEntry

--- scripts/build/write-book-entry.js
+++ scripts/build/write-book-entry.js
@@ -1,5 +1,6 @@
 import { marked } from "marked";
 import fs from "fs";
+import path from "path";
 
 const ENCODING = "utf8";
 const OUTPUT_DIRECTORY = "docs";
@@ -9,10 +10,20 @@ const AHERO_DIRECTORY = "ahero";
 const BOOKS_REMOTE =
   "https://raw.githubusercontent.com/carlcidromero/content/refs/heads/main/books";
 
-const AHERO_PATH = `${OUTPUT_DIRECTORY}/${BOOKS_DIRECTORY}/${AHERO_DIRECTORY}`;
+const AHERO_LOCAL = path.join(
+  OUTPUT_DIRECTORY,
+  BOOKS_DIRECTORY,
+  AHERO_DIRECTORY
+);
+const AHERO_REALITY_TEMPLATE_PATH = path.join(
+  "templates",
+  "books",
+  "ahero",
+  "reality",
+  "index.html"
+);
 const AHERO_REMOTE = `${BOOKS_REMOTE}/ahero`;
 const AHERO_REALITY_REMOTE = `${AHERO_REMOTE}/reality.md`;
-const AHERO_REALITY_TEMPLATE_PATH = "templates/books/ahero/reality/index.html";
 
 export async function writeBookEntry() {
   const aheroRealityResponse = await fetch(AHERO_REALITY_REMOTE);
@@ -28,7 +39,7 @@ export async function writeBookEntry() {
     `<article data-src="books\/ahero\/reality.md">${aheroRealityParsed}<\/article>`
   );
 
-  fs.mkdirSync(`${AHERO_PATH}/reality`, { recursive: true });
+  fs.mkdirSync(`${AHERO_LOCAL}/reality`, { recursive: true });
 
-  fs.writeFileSync(`${AHERO_PATH}/reality/index.html`, aheroRealityHtml);
+  fs.writeFileSync(`${AHERO_LOCAL}/reality/index.html`, aheroRealityHtml);
 }
